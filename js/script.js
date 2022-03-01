c31dd7fb906a9a2ff191c435a56bd59531c41411
Justine-Derrick <justinederrick.0496@gmail.com>
2022-03-01 16:55:58 +1300

    layout and styling changes, api added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("howdy");
+
+// let lang = "en";
+let newsAPI = "https://gnews.io/api/v4/search?lang=en&token=" + key;
+let topStoriesAPI = "https://gnews.io/api/v4/top-headlines?lang=en&token=" + key;
+
+
+
+$.ajax({
+    method: "GET",
+    url: topStoriesAPI,
+    success: function(result){
+        
+        console.log(result);
+    }
+});
